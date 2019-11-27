1c228d715b80c9c3bb65f9920df01b5cb42ad8fd
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2019-11-27 20:00:01 +0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -98,6 +98,16 @@ def get_keyboard_reply_markup(btn):
         reply_markup = keyboard("9")
     if get_config()['button_belong_keyboard'][btn] == "keyboard_10":
         reply_markup = keyboard("10")
+    if get_config()['button_belong_keyboard'][btn] == "keyboard_11":
+        reply_markup = keyboard("11")
+    if get_config()['button_belong_keyboard'][btn] == "keyboard_12":
+        reply_markup = keyboard("12")
+    if get_config()['button_belong_keyboard'][btn] == "keyboard_13":
+        reply_markup = keyboard("13")
+    if get_config()['button_belong_keyboard'][btn] == "keyboard_14":
+        reply_markup = keyboard("14")
+    if get_config()['button_belong_keyboard'][btn] == "keyboard_15":
+        reply_markup = keyboard("15")
 
     return reply_markup
 
@@ -110,69 +120,69 @@ def get_order_data(message):
         bot.register_next_step_handler(message, cancel)
     else:
         send_typing(message)
-        bot.send_message(message.from_user.id, text=get_config()['order_data']['ord1'])
+        bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_1'])
         zero = message.text
         bot.register_next_step_handler(message, get_order_data_0, zero)
 
 def get_order_data_0(message, zero):
     one = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord2'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_2'])
     bot.register_next_step_handler(message, get_order_data_1, zero, one)
 
 
 def get_order_data_1(message, zero, one):
     two = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord3'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_3'])
     bot.register_next_step_handler(message, get_order_data_2, zero, one, two)
 
 def get_order_data_2(message, zero, one, two):
     three = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord4'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_4'])
     bot.register_next_step_handler(message, get_order_data_3, zero, one, two, three)
 
 
 def get_order_data_3(message, zero, one, two, three):
     four = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord5'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_5'])
     bot.register_next_step_handler(message, get_order_data_4, zero, one, two, three, four)
 
 
 def get_order_data_4(message, zero, one, two, three, four):
     five = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord6'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_6'])
     bot.register_next_step_handler(message, get_order_data_5, zero, one, two, three, four, five)
 
 
 def get_order_data_5(message, zero, one, two, three, four, five):
     six = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord7'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_7'])
     bot.register_next_step_handler(message, get_order_data_6, zero, one, two, three, four, five, six)
 
 
 def get_order_data_6(message, zero, one, two, three, four, five, six):
     seven = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord8'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_8'])
     bot.register_next_step_handler(message, get_order_data_7, zero, one, two, three, four, five, six, seven)
 
 
 def get_order_data_7(message, zero, one, two, three, four, five, six, seven):
     eight = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord9'])
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['ord_question_9'])
     bot.register_next_step_handler(message, get_order_data_8, zero, one, two, three, four, five, six, seven, eight)
 
 
 def get_order_data_8(message, zero, one, two, three, four, five, six, seven, eight):
     nine = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=get_config()['add_vars']['thanks_for_order'], reply_markup=get_keyboard_reply_markup('btn1'))
+    bot.send_message(message.from_user.id, text=get_config()['order_data']['thanks_for_order'], reply_markup=get_keyboard_reply_markup('btn1'))
     send_typing(message)
     bot.send_message(message.from_user.id, text=f"{zero}, {one}, {two}, {three}, {four}, {five}, {six}, {seven}, {eight}, {nine}", reply_markup=get_keyboard_reply_markup('btn1'))
     # bot.send_message(931750534, text=f"{one}\n{two}\n{three}\n{four}\n{five}\n{six}\n{seven}\n{eight}\n{nine}", reply_markup=get_keyboard_reply_markup('btn1'))
@@ -226,14 +236,14 @@ def get_user_name(message):
     else:
         name = message.text
         send_typing(message)
-        bot.send_message(message.from_user.id, text=f"{get_config()['add_vars']['phone']} ?")
+        bot.send_message(message.from_user.id, text=f"{get_config()['order_data']['phone']} ?")
         bot.register_next_step_handler(message, get_user_phone, name)
 
 
 def get_user_phone(message, name):
     phone = message.text
     send_typing(message)
-    bot.send_message(message.from_user.id, text=f"{get_config()['add_vars']['name']}: {name}\n{get_config()['add_vars']['phone']}: {phone}", reply_markup=get_keyboard_reply_markup('btn1'))
+    bot.send_message(message.from_user.id, text=f"{get_config()['order_data']['name']}: {name}\n{get_config()['order_data']['phone']}: {phone}", reply_markup=get_keyboard_reply_markup('btn1'))
     # bot.send_message(931750534, text=f"{name} {phone}", reply_markup=get_keyboard_reply_markup('btn1'))
 
 
